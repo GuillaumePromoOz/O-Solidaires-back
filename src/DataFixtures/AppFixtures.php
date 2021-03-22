ad82b17a90b19a41296daf9395b92118d33128f2
Thomas Viret <th.viret@gmail.com>
2021-03-22 16:50:55 +0100

    fixtures ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,8 +4,10 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\User;
+use App\Entity\Request;
 use App\Entity\Category;
 use App\Entity\Department;
+use App\Entity\Proposition;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use App\DataFixtures\Provider\OsolidaireProvider;
@@ -23,8 +25,7 @@ class AppFixtures extends Fixture
         
     }
     
-    const NB_DEPARTMENTS = 5;
-    const NB_CATEGORIES = 5;
+    
     const NB_USERS = 10;
     const NB_PROPOSITIONS = 2 * self::NB_USERS;
     const NB_REQUESTS = 2 * self::NB_USERS;
@@ -118,6 +119,77 @@ class AppFixtures extends Fixture
 
             $manager->persist($user);
         }
+
+        //--- ADMIN ---//
+
+        $adminsList = [];
+        for ($i=1; $i <= 2; $i++) {
+            
+            $user = new User();
+            $plainPassword = 'Az123456';
+            $encoded = $this->passwordEncoder->encodePassword($user,$plainPassword);
+            $user->setPassword($encoded);
+            $user->setLastname($faker->lastName());
+            $user->setFirstname($faker->firstName());
+            
+            
+            $user->setEmail('toto'.$i.'@gmail.com');
+            $user->setRoles(['ROLE_ADMIN']);
+
+            $randomDepartement = $departementsList[array_rand($departementsList)];
+            $user->setDepartment($randomDepartement);
+
+            $user->setCreatedAt(new \DateTime());
+            
+            $adminsList[] = $user;
+
+            $manager->persist($user);
+        }
+
+        //--- PROPOSITION ---//
+
+        $propositionsList = [];
+        for ($i=1; $i <= self::NB_PROPOSITIONS; $i++) {
+            
+            $proposition = new Proposition();
+            $proposition->setTitle($faker->title());
+            $proposition->setContent($faker->content());
+            $proposition->setDisponibilityDate(new \DateTime());
+            $proposition->setCreatedAt(new \DateTime());
+
+            $randomVolunteer = $volunteersList[array_rand($volunteersList)];
+            $proposition->setUser($randomVolunteer);
+
+            $randomCategory = $categoriesList[array_rand($categoriesList)];
+            $proposition->setCategory($randomCategory);
+            
+            $propositionsList[] = $proposition;
+
+            $manager->persist($proposition);
+        }
+
+        //--- REQUEST ---//
+
+        $requestsList = [];
+        for ($i=1; $i <= self::NB_REQUESTS; $i++) {
+            
+            $request = new Request();
+            $request->setTitle($faker->title());
+            $request->setContent($faker->content());
+            $request->setInterventionDate(new \DateTime());
+            $request->setCreatedAt(new \DateTime());
+
+            $randomBeneficiary = $beneficiariesList[array_rand($beneficiariesList)];
+            $request->setUser($randomBeneficiary);
+
+            $randomCategory = $categoriesList[array_rand($categoriesList)];
+            $request->setCategory($randomCategory);
+            
+            $requestsList[] = $request;
+
+            $manager->persist($request);
+        }
+
 
         // $product = new Product();
         // $manager->persist($product);
