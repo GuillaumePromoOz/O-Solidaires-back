7e1716a0b713a9a2c740f4242ad1bd55b435e055
Guillaume Charneau <guillaume.promo.oz@gmail.com>
2021-03-25 14:04:21 +0100

    access control ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,12 +14,24 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $user = $this->getUser();
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
-        return $this->json([
-            'email' => $user->getEmail(),
-            'roles' => $user->getRoles(),
-        ]);
+        //POST
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error' => $error
+
+            ]
+        );
     }
 
     /**
