beb0a209cffbaaf1aeb055eb2f1289277a2bf55e
Thomas Viret <th.viret@gmail.com>
2021-03-30 16:13:44 +0200

    method add proposition back office ok

--- src/Form/PropositionType.php
+++ src/Form/PropositionType.php
@@ -6,15 +6,27 @@ use App\Entity\Proposition;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PropositionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('disponibilityDate')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'
+                 
+            ])
+            ->add('disponibilityDate', DateType::class, [
+                'placeholder' => [
+                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+                ]
+            ])
             ->add('user')
             ->add('category')
         ;
