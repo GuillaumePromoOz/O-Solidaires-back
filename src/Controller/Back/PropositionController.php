1f3259cd67c2a1250b7ca0c322b802fbb0bb88b6
Thomas Viret <th.viret@gmail.com>
2021-03-30 11:22:39 +0200

    PropositionController BackOffice wip

--- src/Controller/Back/PropositionController.php
+++ src/Controller/Back/PropositionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Back;
 
+use App\Repository\PropositionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class PropositionController extends AbstractController
 {
     /**
-     * @Route("/back/proposition", name="back_proposition")
+     * Get all propositions
+     * 
+     * @Route("/back/proposition/browse", name="back_proposition_browse", methods={"GET"})
      */
-    public function index(): Response
+    public function browse(PropositionRepository $propositionRepository): Response
     {
-        return $this->render('back/proposition/index.html.twig', [
-            'controller_name' => 'PropositionController',
+        $propositions = $propositionRepository->findAll();
+
+        return $this->render('back/proposition/browse.html.twig', [
+            'propositions' => $propositions,
         ]);
     }
 }
