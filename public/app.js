04d5129bd01c1c6db581bee68a9da895e13d6d64
Guillaume Charneau <guillaume.promo.oz@gmail.com>
2021-04-06 09:16:32 +0200

    background theme wip

---
+++ public/app.js
@@ -0,0 +1,27 @@
+navItem = document.querySelector('.container-dark');
+//console.log(navItem);
+
+const init = function() {
+
+    navItem.addEventListener('click', handleBodyColorSwitch);
+};
+
+const handleBodyColorSwitch = function(event) {
+    // récupération de du bouton validation (il a déclenché l'event)
+    event.currentTarget.preventDefault;
+
+    let bodyElement = document.querySelector('.theme');
+
+    const themes = ["dark1", "dark2", "dark3",];
+
+    const random = Math.floor(Math.random() * themes.length);
+
+    bodyElement.classList.add('theme--' + themes[random]);
+    bodyElement.classList.remove('theme--dark0');
+
+   
+
+};
+
+document.addEventListener('DOMContentLoaded', init);
+
